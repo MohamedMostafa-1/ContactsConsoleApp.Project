6b10056ef5b086a3f26c8d2b197a093159d0c58e
MohamedMostafa-1 <mohamedmostafajalal@gmail.com>
2026-09-19 12:35:44 +0300

    feature(Find Contact using 3 Tier Architecture)

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using ContactsBusinessLayer;
+
+
+namespace ContactsConsoleApp_PresentationLayer
+{
+    internal class Program
+    {
+
+        static void testFindContact(int ID)
+        {
+            clsContact contact = clsContact.Find(ID);
+
+            if(contact != null)
+            {
+                Console.WriteLine("ID: " + contact.ID);
+                Console.WriteLine("FirstName: " + contact.FirstName);
+                Console.WriteLine("LastName: " + contact.LastName);
+                Console.WriteLine("Email: " + contact.Email);
+                Console.WriteLine("Phone: " + contact.Phone);
+                Console.WriteLine("Address: " + contact.Address);
+                Console.WriteLine("ImagePathe: " + contact.ImagePath);
+                Console.WriteLine("CountryID: " + contact.CountryID);
+                Console.WriteLine("DateOfBirth: " + contact.DateOfBirth);
+            }
+            else
+            {
+                Console.WriteLine("Contact [" + ID + "] Not Found");   
+            }
+        }
+        static void Main(string[] args)
+        {
+            testFindContact(1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ContactsConsoleApp_PresentationLayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContactsConsoleApp_PresentationLayer")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5735e25-7334-4434-8b3a-f13a318b1587")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
